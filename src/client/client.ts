8f9bd67a5f58b39f178057ec2fccaa360e5084fa
Den Pol <selection4test@gmail.com>
2021-01-02 01:29:26 +0300

    lesson 13: OrbitControls

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,5 @@
 import * as THREE from '/build/three.module.js'
+import { OrbitControls } from '/jsm/controls/OrbitControls'
 
 const scene = new THREE.Scene()
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
@@ -6,6 +7,8 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize( window.innerWidth, window.innerHeight )
 document.body.appendChild( renderer.domElement )
+
+const controls = new OrbitControls(camera, renderer.domElement)
 
 const geometry = new THREE.BoxGeometry()
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } )
@@ -19,6 +22,8 @@ function animate() {
   
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
+
+  controls.update()
 
 	renderer.render( scene, camera )
 }
